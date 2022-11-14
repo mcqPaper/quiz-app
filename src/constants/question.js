5b80763111a99de6b20b0d7b63c843d5617802dd
ccnextgen <103283908+ccnextgen@users.noreply.github.com>
2022-11-14 22:39:22 +0530

    all

---
+++ src/constants/question.js
@@ -0,0 +1,66 @@
+const QUESTIONS = {
+    "response_code": 0,
+    "results": [
+        {
+            "category": "General Knowledge",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the name given to Indian food cooked over charcoal in a clay oven?",
+            "correct_answer": "Tandoori",
+            "incorrect_answers": [
+                "Biryani",
+                "Pani puri",
+                "Tiki masalaaaaaaaaaaaaaaaaaaaaaa"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the romanized Japanese word for &quot;university&quot;?",
+            "correct_answer": "Daigaku",
+            "incorrect_answers": [
+                "Toshokan",
+                "Jimusho",
+                "Shokudou"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the romanized Russian word for &quot;winter&quot;?",
+            "correct_answer": "Zima",
+            "incorrect_answers": [
+                "Leto",
+                "Vesna",
+                "Osen&#039;"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the last letter of the Greek alphabet?",
+            "correct_answer": "Omega",
+            "incorrect_answers": [
+                "Mu",
+                "Epsilon",
+                "Kappa"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In a 1994 CBS interview, Microsoft co-founder Bill Gates performed what unusual trick on camera?",
+            "correct_answer": "Jumping over an office chair",
+            "incorrect_answers": [
+                "Jumping backwards over a desk",
+                "Standing on his head",
+                "Typing on a keyboard during a handstand"
+            ]
+        }
+    ]
+}
+export default QUESTIONS;
